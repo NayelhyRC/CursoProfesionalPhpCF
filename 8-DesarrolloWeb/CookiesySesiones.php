7cf4567e3a4017f51a21f78d0e4e9dfe05212cee
Juan Kaenel <kaenel10@gmail.com>
2020-06-05 01:45:59 -0300

    Teoría de Cookies y Sesiones y modificación de la descripción del README

---
+++ 8-DesarrolloWeb/CookiesySesiones.php
@@ -0,0 +1,16 @@
+<?php
+/*
+Las cookies almacenan datos del lado del cliente.
+Las sesiones almacenan datos del lado del servidor pero sesiones utilizan cookies.
+
+Una sesion es una cantidad de informacion q se almacena dentro del sv con un id unico de usuario. Este es enviado automáticamente a un usuario por medio de las cookies
+dentro de estas sesiones se pueden almacenar valores de las variables y disponer de ellas en cualquiera de las páginas en la q se inicie sesión. una ventaja extra es q al cambiar
+el valor de las variables automaticamente cambia el valor para sus sesiones. Lo más importante entre la relación entre cookies y sesiones es que un sv puede identificar
+automáticamente un usuario por medio del identificador
+
+En un cookie se puede almacenar pocos datos. Hasta 4093 caracteres por cada cookie y solo es posible almacenar hasta 20 cookies por dominio
+Un navegador solo puede almacenar hasta 300 cookies en total compartidas entre todos los sitios que el usuario visita, de lo contrario irá borrando a medida q necesita
+
+ALMACENAR EL USR Y CONTRASEÑA EN UNA COOKIE ES MUY PELIGROSA
+
+ */
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# CursoProfesionalPhpCF
-Curso Profesional de Php por Código Facilito
+# Curso Profesional de Php por Código Facilito
+
+En este repositorio están la mayoría de las clases tomadas en este curso, solo que en algunas ocasiones no se tuvieron en cuenta algunas clases debido a que eran de facil entendimiento o ya se contaba con ese aprendizaje. También se reforzó la parte de POO con otras clases.
